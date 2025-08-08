ea6c05f2ee24e8f377ce9d014388d080cec58748
Gabriel <kore.dv7@gmail.com>
2025-08-08 19:55:52 -0300

    Upd about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -10,7 +10,7 @@ export default function AboutPage() {
           <a href="https://en.wikipedia.org/wiki/Rio_de_Janeiro" target="_blank">Rio de Janeiro</a>, Brazil. From an early age, I was curious about how things worked, and I found in programming the perfect tool to bring ideas to life.
         </p>
         <p>
-          I wrote my first lines of code at the age of 13, driven by the desire to solve small daily problems with technology. Since then, coding became more than just a skill, it became my passion and career.
+          I wrote my first lines of code at the age of 13, driven by the desire to solve small daily problems with technology. By age 14, I had already dedicated a large part of my day to programming. Since then, coding became more than just a skill, it became my passion and career.
         </p>
         <p>
           Over the past years, I&apos;ve worked professionally as a fullstack developer, mostly with{" "}
